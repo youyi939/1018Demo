93c6811bf813c7891b4b6b06f07c95ae4c25be69
youyi <1291174818@qq.com>
2019-10-22 09:23:22 +0800

    网络抓取json数据渲染到listView

---
+++ app/src/main/java/com/example/a1018demo/TwoFragment.java
@@ -0,0 +1,140 @@
+package com.example.a1018demo;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class TwoFragment extends Fragment {
+
+    StringBuilder response;
+    InputStream in;
+    HttpURLConnection connection = null;
+    BufferedReader reader = null;
+    private ListView listView;
+    private ListViewAdapter adapter;
+    private List<Goods> goods = new ArrayList<>();
+    private OkHttpClient okHttpClient;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        listView = view.findViewById(R.id.listView);
+        okHttpClient = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS)          //设置连接超时时间
+                .readTimeout(20, TimeUnit.SECONDS)                      //设置读取超时时间
+                .build();;
+        String url = "https://api.myjson.com/bins/8ru3p";
+        getJson(url);
+        return view;
+    }
+
+    private void getJson(String url){
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {                       //不写也没事
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                showInListview(json);
+            }
+        });             //网络抓取过程
+    }               //okhttp
+
+    private void getJson2(final String url){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL url1 = new URL(url);
+                    connection = (HttpURLConnection) url1.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                   in = connection.getInputStream();
+
+                   reader = new BufferedReader(new InputStreamReader(in));
+                   response = new StringBuilder();
+                   String line;
+                   while ((line = reader.readLine()) != null){
+                       response.append(line);
+                   }
+                   showInListview(response.toString());
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (reader != null){
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null){
+                        connection.disconnect();
+                    }
+                }
+
+            }
+        }).start();
+    }        //原生
+
+    private void showInListview(final String responce){
+        try {
+            JSONArray jas = new JSONArray(responce);
+            for (int i = 0; i < jas.length(); i++) {
+                JSONObject jsonObject = jas.getJSONObject(i);
+                String nameJson = jsonObject.getString("name");
+                String priceJson = jsonObject.getString("price");
+                final String imageJson = jsonObject.getString("imageUrl");
+
+                goods.add(new Goods(nameJson, priceJson, imageJson));
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter = new ListViewAdapter(getContext(), R.layout.item, goods);
+                        adapter.notifyDataSetChanged();
+                        listView.setAdapter(adapter);
+                    }
+                });
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
