d30b9afd05b26a4643ac0321acb0d6d9966dae9e
youyi <1291174818@qq.com>
2019-10-22 09:53:58 +0800

    网络抓取json数据渲染到listView

--- app/src/main/java/com/example/a1018demo/ListViewAdapter.java
+++ app/src/main/java/com/example/a1018demo/ListViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.a1018demo;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,10 +9,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class ListViewAdapter extends ArrayAdapter<Goods> {
-
+    Bitmap bitmap = null;
     private int resourceId;
 
     public ListViewAdapter(Context context, int resource, List objects) {
@@ -30,8 +38,41 @@ public class ListViewAdapter extends ArrayAdapter<Goods> {
         name.setText("商品名称:\n" + good.getName());
         price.setText("商品价格:\n" + good.getPrice());
 
-        Glide.with(parent.getContext()).load(good.getImage()).into(imageView);
+        //Glide.with(parent.getContext()).load(good.getImage()).into(imageView);              //Glide框架
+
+        imageView.setImageBitmap(returnBitmap(good.getImage()));
+
 
         return view;
     }
+
+    private Bitmap returnBitmap(final String url){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL fileUrl = null;
+
+                try {
+                    fileUrl = new URL(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) fileUrl.openConnection();
+                    connection.setDoInput(true);
+                    connection.connect();
+                    InputStream in = connection.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(in);
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        return  bitmap;
+    }           //原生根据url渲染图片
+
 }
